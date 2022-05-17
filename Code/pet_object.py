8fe3b1eee21298f9a316ebc9b7d813d99217bb31
teverus <teverus@yandex.ru>
2022-05-17 07:23:59 +0300

    code review 4

--- Code/pet_object.py
+++ Code/pet_object.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 from Code.constants import PetStatus
 
@@ -6,10 +7,11 @@ from Code.constants import PetStatus
 @dataclass
 class Pet:
     """Dataclass for a pet"""
+
     pet_name: str
-    photo_urls: list = None
+    photo_urls: List = None
     pet_id: int = None
     category_id: int = None
     category_name: str = None
-    tags: list = None
+    tags: List = None
     status: PetStatus = None
